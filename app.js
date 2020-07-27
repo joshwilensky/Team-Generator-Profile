0b9e649c15bdafba0984dde0ca81c9ac2d1e2b5f
Joshua Wilensky <joshwilensky@gmail.com>
2020-07-27 17:37:51 -0400

    async function

--- app.js
+++ app.js
@@ -71,9 +71,11 @@ const employeeQuestions = [
     name: "addEmployee",
     message: "Choose which position for the new employee:",
     type: "list",
-    choices: ["Manager", "Engineer", "Intern", "Stop Adding More Employees"],
+    choices: ["Manager", "Engineer", "Intern", "Don't add anymore employees"],
   },
 ];
+
+async function init() {}
 // After the user has input all employees desired info, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
