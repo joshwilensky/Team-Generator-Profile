782da3499bff8c255376d33e2afa3591e5312df5
Joshua Wilensky <joshwilensky@gmail.com>
2020-07-27 16:32:53 -0400

    Added a new employee array with questions to app.js

--- app.js
+++ app.js
@@ -4,16 +4,30 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const jest = require("jest");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeArray = new Array();
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
+/* Write code to use inquirer to gather information about the development team members,
+ and to create objects for each team member (using the correct classes as blueprints!)*/
+const employeeQuestions = [
+  { name: "name", message: "Enter the employee's name:", type: "input" },
+  { name: "id", message: "Enter the employee's ID:", type: "input" },
+  {
+    name: "email",
+    message: "Enter the employee's e-mail:",
+    type: "input",
+    validate: function (email) {
+      var re = /\S+@\S+\.\S+/;
+      return re.test(email);
+    },
+  },
+];
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
